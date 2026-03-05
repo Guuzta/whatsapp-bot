f192050275655700b96e0caebc9faa0f8d614571
Guuzta <Guuhsilva13@outlook.com>
2026-03-05 17:13:35 -0300

    configurando eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  {
+    ignores: ["dist", "node_modules"],
+  },
+
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: "tsconfig.json",
+      },
+    },
+  },
+);
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,4 @@ const app = (): void => {
   console.log("Hello world");
 };
 
-const teste = "Gustavo";
-
 app();
