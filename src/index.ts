0de2ceb3ca3b6d50bffed56ee5af016f912ddb85
Guuzta <Guuhsilva13@outlook.com>
2026-03-07 10:42:35 -0300

    estrutura inicial do bot

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-const app = (): void => {
-  console.log("Hello world");
-};
+import { Client, LocalAuth } from "whatsapp-web.js";
+import qrcode from "qrcode-terminal";
 
-app();
+const client = new Client({
+  authStrategy: new LocalAuth(),
+});
+
+client.once("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("qr", (qr: string) => {
+  console.log("QR RECEIVED");
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("message_create", async (message) => {
+  const text = message.body;
+
+  if (text === "!ping") {
+    await message.reply("pong");
+  }
+});
+
+client.initialize();
